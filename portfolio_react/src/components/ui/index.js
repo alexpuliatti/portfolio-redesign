1ba1d638e37e5e1cb6c2a356b04bea9ae43269d4
duchamps0305 <a@puliatti.com>
2026-05-11 17:11:38 +0100

    Remove gradient component library files from portfolio repo

--- portfolio_react/src/components/ui/index.js
+++
@@ -1,4 +0,0 @@
-export { GradientLine } from './GradientLine';
-export { GradientDivider } from './GradientDivider';
-export { GradientBorder } from './GradientBorder';
-export { GradientUnderline } from './GradientUnderline';
